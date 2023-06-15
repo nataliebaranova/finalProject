f3ad452bb3f8ef12c73ddad9d33a2149f58192ee
nataliebaranova <123493724+nataliebaranova@users.noreply.github.com>
2023-06-15 17:39:18 +0200

    final

--- script.js
+++ script.js
@@ -1,53 +1,38 @@
-const nameInput = document.getElementById("nameID");
-const emailInput = document.getElementById("emailID");
-const messageInput = document.getElementById("messageID");
-
-const button = document.getElementById("buttonID");
 
 
-button.addEventListener('click', function() {
+window.addEventListener('DOMContentLoaded', function () {
+  var hiddenLink = document.getElementById('hidden-link');
+  hiddenLink.classList.add('hidden');
+});
 
-const nameValue = nameInput.value;
+/* window.addEventListener('DOMContentLoaded', function () {
+  var introWindow = document.getElementById('intro-window');
+  introWindow.classList.add('hide');
 
-const emailValue = emailInput.value;
-
-const messageValue = messageInput.value;
-
-if (nameValue === '') {
-  nameInput.alert = 'Name must be input';
-} else {
-  nameInput.alert = '';
-}
-
-if (emailValue === '') {
-  emailInput.alert = 'E-mail must be input';
-} else {
-  emailInput.alert = '';
-}
-if (messageValue === '') {
-  messageInput.alert = 'Message must be input';
-} else {
-  messageInput.alert = '';
-}
-
-if (nameValue === '' || emailValue === '' || messageValue === '') {
-  return
-}
-alert('Thank you')
-
-}
-
-);
-
+  setTimeout(function () {
+    introWindow.remove();
+  }, 10000);
+});
+*/
 
 function sendEmail() {
-  Email.send({
-    Host: "smtp.gmail.com",
-    To: 'receiver@email_address.com',
-    From: "sender@email_address.com",
+  var message = document.getElementById("messageID").value;
+  var name = document.getElementById("nameID").value;
+  var email = document.getElementById("emailID").value;
 
-  })
-    .then(function (message) {
-      alert("Message sent successfully!")
-    });
+  if (message !== "" && name !== "" && email !== "") {
+    alert("Your message was sent successfully!");
+  } else {
+    alert("Please fill in all the required fields.");
   }
+
+  var subject = "New email from " + name;
+  var body = "Name: " + name + "\nEmail: " + email + "\n\nMessage:\n" + message;
+
+
+  sendEmailRequest(subject, body);
+
+  alert("Your message was sent successfully!");
+}
+
+document.getElementById("sendButton").addEventListener("click", sendEmail);
